02fc412e518168f945ae1a57f4473f86bbce20c8
FaqihAhnaff <93029845+inipunyafaqih@users.noreply.github.com>
2022-01-05 16:10:57 +0700

    Add files via upload

---
+++ praktikum9/otentikasi.js
@@ -0,0 +1,12 @@
+const login = document.querySelector('#login');
+  login.addEventListener('click', function(){
+    const username = document.querySelector('#username').value;
+    const password = document.querySelector('#password').value;
+    
+    if(username == "faqih" && password == "terserahaja"){
+      alert('benar')
+      document.body.innerHTML="<h1>ANDA BERHASIL LOGIN</h1>"
+    }else{
+      alert('password salah\nmasukkan\nusername: dinda\npassword: 12345')
+    }
+  })
